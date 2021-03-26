93dbe2884e360e7461c2739afd776240c3e14ebf
Marta <55.mrt.22@gmail.com>
2021-03-26 09:37:35 +0100

    fixed 1st heroku errors

--- app.js
+++ app.js
@@ -192,6 +192,7 @@ app.use( (err, req, res, next) => {
 })
 
 /**************** SERVER START ****************/
-app.listen(3000, () => {
-    console.log('Serving app on localhost:3000')
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Serving app on port ${port}`)
 })
